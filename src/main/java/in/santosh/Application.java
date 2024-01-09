f14b4403ff36b17497a9eb1fdda4fb66cbe77ee7
Santosh <info.santosh3851@gmail.com>
2024-01-10 01:25:03 +0530

    p-1 sprint work completed

--- src/main/java/in/santosh/Application.java
+++ src/main/java/in/santosh/Application.java
@@ -2,12 +2,15 @@ package in.santosh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		//Working for p-1 Sprint
+		run.close();
 	}
 
 }
